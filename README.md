2ca0007c68d91deef38cd3f5ff2e69a0205eaeb8
LODYGENSKY Oleg <oleg.lodygensky@lal.in2p3.fr>
2024-11-20 21:51:54 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# PEFT 
+
+A first project to understand and test large pretrained model adaptation with 
+PEFT (Parameter-Efficient Fine-Tuning).
+
+# Ref
+[Higging Face Parameter-Efficient Fine-Tuning page](https://huggingface.co/docs/peft/main/en/index)
+## Config
+```
+ python3.12 -m venv .
+ source bin/activate
+ pip install --upgrade pip
+ pip install peft
+```
+
+
---
+++ src/peft_lora.py
@@ -0,0 +1,14 @@
+from transformers import AutoModelForCausalLM
+from peft import LoraConfig, get_peft_model, PeftConfig
+import json
+
+model = AutoModelForCausalLM.from_pretrained("facebook/opt-350m")
+
+with open('lora_config.json', 'r') as f:
+    lora_config_dict = json.load(f)
+
+print(lora_config_dict)
+lora_config = LoraConfig(**lora_config_dict)
+
+lora_model = get_peft_model(model, lora_config)
+lora_model.print_trainable_parameters()
